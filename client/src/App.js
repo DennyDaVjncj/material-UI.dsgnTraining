7cb6ae8820ed7b9bfb49249f4139326c7258f1b2
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-26 03:24:52 -0500

    npm i aftermath

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button>2D surface layer</Button>
+        <Button variant='contained'>
+          2D surface layer
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
