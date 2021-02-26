66ac229b9a1464e43ed2b0a9894ed4e357bd64a4
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-26 04:14:07 -0500

    installed icons package

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
+import Icons
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant='contained'>
+        <Button
+         href='#'
+          onClick={()=>alert('da Vjncj dissonance')}
+           variant='outlined'
+            color='secondary'
+            size='small'
+            style={{fontSize:12}}>{/**I can make anything I want to happen, happen on the click of a button|I can even run a function from my store directory|figured out how to comprehend the|can set buttons to a 'Link' */}
           2D surface layer
         </Button>
         <img src={logo} className="App-logo" alt="logo" />
